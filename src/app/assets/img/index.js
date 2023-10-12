9221f74d18a8fdf5f34528ffd98a18b48fbf7a33
Nwabuisi Obinna Divine <obinnadivine107@gmail.com>
2023-10-12 15:57:48 +0100

    code clean up

--- src/app/assets/img/index.js
+++
@@ -1,5 +0,0 @@
-import headerLogo from './header-logo.svg'
-
-export {
-    headerLogo
-}
--- src/app/components/Nav.js
+++ src/app/components/Nav.js
@@ -1,21 +1,18 @@
 import React from 'react'
-import { hamburger } from "../assets/icons/index";
-import { headerLogo } from "../assets/img";
+import { hamburger } from "../assets/icons";
 
 const Nav = () => {
   return (
-    <header className='padding-x py-8 absolute z-10 w-full bg-red-300'>
-            <nav className='flex justify-around bg-green-500 items-center max-container'>
+    <header className='padding-x py-6 border-[1px]  absolute z-10 w-full '>
+            <nav className='flex justify-around items-center max-container'>
                 <a href='/'>
-                    <img
-                        src={headerLogo}
+                    <img src=''/>
+                    <h1
                         alt='logo'
-                        width={129}
-                        height={29}
-                        className='m-0 w-[130px] h-[29px]'
-                    />
+                        className='text-2xl font-bold'
+                    >SafetySentry</h1>
                 </a>
-                <div className='flex gap-2 text-lg py-4 px-4 rounded-[40px] text-white bg-[#FF6451] leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
+                <div className='flex gap-2 text-lg py-4 px-4 rounded-[40px] text-white bg-[#3AABE2] leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
                     <a href='/'>Sign in</a>
                     <span>/</span>
                     <a href='/'>Explore now</a>
