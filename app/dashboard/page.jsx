2e20981689906a78b5ec98b31284541a0257e5b6
Nwabuisi Obinna Divine <obinnadivine107@gmail.com>
2023-11-02 13:31:53 +0100

    debugging code

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,17 +1,32 @@
+'use client';
+
 import { useSession } from 'next-auth/react';
+import { useState } from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 const Dashboard = () => {
-  const { data: session } = useSession();
-
-  if (!session) {
-    return <div>Please sign up or log in to view the dashboard.</div>;
-  }
-
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome, {session.user.name}!</p>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider width={80} theme="light">
+        {/* Sidebar content */}
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+          {/* Header content */}
+        </Header>
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            {/* Breadcrumb content */}
+          </Breadcrumb>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            {/* Dashboard content */}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Your Footer Text</Footer>
+      </Layout>
+    </Layout>
   );
 };
 
